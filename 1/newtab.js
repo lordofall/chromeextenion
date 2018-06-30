5bd86308fcc92e4b0447c40440106904f09b4f9f
CORP\Q4465 <akhilesh.kumar@qualitykiosk.com>
2018-07-01 00:03:41 +0530

    test

--- 1/newtab.js
+++ 1/newtab.js
@@ -1,13 +1,16 @@
 function startTime() {
 	var today = new Date();
-	var h = today.getHours();
-	var m = today.getMinutes();
-	var s = today.getSeconds();
-	m = checkTime(m);
-	s = checkTime(s);
-	document.getElementById('txt').innerHTML = new Date() + " "
-	h + ":" + m + ":" + s;
-	var t = setTimeout(startTime, 100);
+	//var h = today.getHours();
+	//var m = today.getMinutes();
+	//var s = today.getSeconds();
+	var date = today.getDate();
+	var month = today.getMonth();
+	var year = today.getFullYear();
+	date = checkTime(date);
+	month = checkTime(month);
+	//s = checkTime(s);
+	document.getElementById('txt').innerHTML = 	date + "." + month + "." + year;
+	//var t = setTimeout(startTime, 100);
 }
 function checkTime(i) {
 	if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
